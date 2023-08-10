f99fd6aafa96b00bcda2276851433968a3719e7d
Selasi-Mensah <mensahselasi78@gmail.com>
2023-08-10 09:39:54 -0700

    Sweet

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-/* 4. Using put in a code 
+/** 4. Using put in a code 
  *  Filename is 4-puts.c
  *  A c program
  *  Uses compiler
  */
 #include <stdio.h>
-/*
+/**
  * main code- This prints what is in the braces
  * Then starts a new line
  */
