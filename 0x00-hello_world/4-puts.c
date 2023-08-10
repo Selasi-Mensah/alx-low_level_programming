35d0bfe12bc9766f0c808a1cda9ffc2107d4bacc
Selasi-Mensah <mensahselasi78@gmail.com>
2023-08-10 16:05:47 +0000

    Hope it works

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,16 @@
+/* 4. Using put in a code 
+ *  Filename is 4-puts.c
+ *  A c program
+ *  Uses compiler
+ */
 #include <stdio.h>
+/*
+ * main code- This prints what is in the braces
+ * Then starts a new line
+ */
 
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+    return( 0);
 }
