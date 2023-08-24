4ad125ad99fef31567254af3ea9583b5e08a3643
Selasi-Mensah <mensahselasi78@gmail.com>
2023-08-24 12:22:06 +0000

    First one

--- 0-strcat.c
+++ 0-strcat.c
@@ -10,19 +10,14 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    char *ptr = dest;    
-    while (*ptr != '\0') 
-    {
-        ptr++;
-    }
-    while (*src != '\0') 
-    {
-        *ptr = *src;
-        ptr++;
-        src++;
-    }
-    
-    *ptr = '\0';
-    
-    return (dest);
+
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
 }
